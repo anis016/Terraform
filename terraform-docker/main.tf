0c7da5e05330a7e017f7bad8d24867ab9c13df84
Sayed Anisul Hoque <anis.cuet016@gmail.com>
2021-05-31 00:17:27 +0200

    Removed duplicate random_string and added count

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -19,61 +19,55 @@ resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
+# Create 3 random string
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "random" {
+  count = 3
   length = 4
   special = false
   upper = false
 }
 
-resource "random_string" "random2" {
-  length = 4
-  special = false
-  upper = false
-}
-
-# Create a container
+# Create 3 containers randomly generated using 3 "random_string" resource
 # nodered_container - name of the resource
 # nodered           - name of the docker container
-resource "docker_container" "nodered_container" { 
-  name  = join("-", ["nodered", random_string.random.result])
+resource "docker_container" "nodered_container" {
+  count = 3
+  name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   hostname = "my.nodered.com"
   ports {
     internal = 1880
-    # external = 1880 # let docker setup the port
-  }
-}
-
-# Create another container
-# nodered2_container - name of the resource
-# nodered2           - name of the docker container
-resource "docker_container" "nodered_container2" { 
-  name  = join("-", ["nodered", random_string.random2.result])
-  image = docker_image.nodered_image.latest
-  hostname = "my.nodered.com"
-  ports {
-    internal = 1880
-    # external = 1880 # let docker setup the port
+    # external = 1880 # let docker setup the external port automatically
   }
 }
 
 output "container-name1" {
-  value       = docker_container.nodered_container.name
+  value       = docker_container.nodered_container[0].name
   description = "shows the name of the docker container"
 }
 
 output "container-name2" {
-  value       = docker_container.nodered_container2.name
+  value       = docker_container.nodered_container[1].name
+  description = "shows the name of the docker container"
+}
+
+output "container-name3" {
+  value       = docker_container.nodered_container[2].name
   description = "shows the name of the docker container"
 }
 
 output "ip-address1" {
-  value       = join(":", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].external])
+  value       = join(":", [docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external])
   description = "shows the ip address and the external port of the docker container"
 }
 
 output "ip-address2" {
-  value       = join(":", [docker_container.nodered_container2.ip_address, docker_container.nodered_container2.ports[0].external])
+  value       = join(":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external])
+  description = "shows the ip address and the external port of the docker container"
+}
+
+output "ip-address3" {
+  value       = join(":", [docker_container.nodered_container[2].ip_address, docker_container.nodered_container[2].ports[0].external])
   description = "shows the ip address and the external port of the docker container"
 }
